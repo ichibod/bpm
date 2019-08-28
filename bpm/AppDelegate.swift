6102e6b74e8bc4f3b78f8e63e834af5b1fb69643
Ryan Jones <ryan.jones@slalom.com>
2019-08-28 10:49:49 -0500

    Update for Xcode 10.4
    Fix .gitignore
    Remove cruft

--- bpm/AppDelegate.swift
+++ bpm/AppDelegate.swift
@@ -10,7 +10,7 @@ import Cocoa
 
 extension String {
     func leftPadding(toLength: Int, withPad character: Character) -> String {
-        let newLength = self.characters.count
+        let newLength = count
         if newLength < toLength {
             return String(repeatElement(character, count: toLength - newLength)) + self
         } else {
@@ -20,91 +20,87 @@ extension String {
 }
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+final class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1)
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     
     // load NSUserDefaults
-    let defaults = NSUserDefaults.standardUserDefaults()
-    
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        statusItem.title = "bpm"
+    let defaults = UserDefaults.standard
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        statusItem.button?.title = "bpm"
         
         if let button = statusItem.button {
-            button.action = Selector("clicked:")
+            button.action = #selector(clicked(sender:))
             button.keyEquivalent = ""
         }
         
-        if !defaults.boolForKey("noShowDialogOnStart") {
-            dialogOKCancelNoshow("Instructions", text: ("Control-Click to quit the app.\nAlt-Click to show this window again.\n\nTap to calculate BPM.\n\n\nBuilt by Ben Brook:   www.builtbybenbrook.com"))
+        if !defaults.bool(forKey: "noShowDialogOnStart") {
+            dialogOKCancelNoshow(question: "Instructions", text: ("Control-Click to quit the app.\nAlt-Click to show this window again.\n\nTap to calculate BPM.\n\n\nBuilt by Ben Brook:   www.builtbybenbrook.com"))
         }
     }
     
-    var lastPress = NSDate()
+    var lastPress = Date()
     var avg = 0.0
     var i = 1.0
-    var timer = NSTimer()
-    
-    
-    func dialogOKCancelNoshow(question: String, text: String) -> Bool {
+    var timer = Timer()
+
+    func dialogOKCancelNoshow(question: String, text: String) {
         let myPopup: NSAlert = NSAlert()
         myPopup.messageText = question
         myPopup.informativeText = text
-        myPopup.alertStyle = NSAlertStyle.WarningAlertStyle
-        myPopup.addButtonWithTitle("OK")
+        myPopup.alertStyle = .warning
+        myPopup.addButton(withTitle: "OK")
         
         myPopup.showsSuppressionButton = true
         myPopup.suppressionButton?.title = "Do not show this message on launch"
         
         let res = myPopup.runModal()
         
-        if (myPopup.suppressionButton?.state == 1) {
-            defaults.setObject(true, forKey: "noShowDialogOnStart")
+        if myPopup.suppressionButton?.state == NSControl.StateValue(1) {
+            defaults.set(true, forKey: "noShowDialogOnStart")
         } else {
-            defaults.setObject(false, forKey: "noShowDialogOnStart")
+            defaults.set(false, forKey: "noShowDialogOnStart")
         }
-        
-        if res == NSAlertFirstButtonReturn {        // return based on button selected
-            return true                                //  return 1 for OK
-        }
-        return false                                    //  else return 0 for cancel
     }
-    
+
+    @objc
     func clicked(sender: AnyObject?) {
         let clickEvent = NSApp.currentEvent!  // see what caused calling of the menu action
             // modifierFlags contains a number with bits set for various modifier keys
             // ControlKeyMask is the enum for the Ctrl key
             // AlternateKeyMask is the enum for the Alt key
             // use logical and with the raw values to find if the bit is set in modifierFlags
-        if (Int(clickEvent.modifierFlags.rawValue) & Int(NSEventModifierFlags.ControlKeyMask.rawValue)) != 0 {      // ctrl click to quit
-            NSApplication.sharedApplication().terminate(self)
-        } else if (Int(clickEvent.modifierFlags.rawValue) & Int(NSEventModifierFlags.AlternateKeyMask.rawValue)) != 0 {     // alt click to show info
-            dialogOKCancelNoshow("Instructions", text: ("Control-Click to quit the app.\nAlt-Click to show this window again.\n\nTap to calculate BPM.\n\nBuilt by Ben Brook:   www.builtbybenbrook.com\n"))
+        if (Int(clickEvent.modifierFlags.rawValue) & Int(NSEvent.ModifierFlags.control.rawValue)) != 0 {      // ctrl click to quit
+            NSApplication.shared.terminate(self)
+        } else if (Int(clickEvent.modifierFlags.rawValue) & Int(NSEvent.ModifierFlags.option.rawValue)) != 0 {     // alt click to show info
+            dialogOKCancelNoshow(question: "Instructions", text: ("Control-Click to quit the app.\nAlt-Click to show this window again.\n\nTap to calculate BPM.\n\nBuilt by Ben Brook:   www.builtbybenbrook.com\n"))
         } else { // regular click
             timer.invalidate()
-            let elapsedTime = NSDate().timeIntervalSinceDate(lastPress)
+            let elapsedTime = Date().timeIntervalSince(lastPress)
             print(elapsedTime)
             if elapsedTime > 2.5 {
                 print("1")
                 avg = 0.0
                 i = 1.0
-                lastPress = NSDate()
+                lastPress = Date()
             } else {
                 print("2")
                 avg = (avg*(i-1) + elapsedTime) / i
-                lastPress = NSDate()
+                lastPress = Date()
                 i = i + 1
                 let x = Int(60/avg)
-                statusItem.title = String(x).leftPadding(toLength: 3, withPad: " ")
+                statusItem.button?.title = String(x).leftPadding(toLength: 3, withPad: " ")
             }
-            timer = NSTimer.scheduledTimerWithTimeInterval(2.5, target:self, selector: Selector("updateCounter"), userInfo: nil, repeats: false)
+            timer = Timer.scheduledTimer(timeInterval: 2.5, target:self, selector: #selector(updateCounter), userInfo: nil, repeats: false)
         }
     }
     
+    @objc
     func updateCounter() {
-        statusItem.title = "bpm"
+        statusItem.button?.title = "bpm"
     }
     
 }
